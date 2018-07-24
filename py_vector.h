ba2a384c94f355f8d1e91a848fae188fe2d480af
David P. Riedel <davidp.riedel@gmail.com>
2018-07-24 14:01:25 -0400

    whole bunch of work one being able to work with different types of variants.
    - use boost Hana for some key parts.

--- py_vector.h
+++ py_vector.h
@@ -25,6 +25,42 @@
 #include <iterator>
 #include <variant>
 #include <vector>
+#include <functional>
+
+#include <boost/hana.hpp>
+
+namespace hana = boost::hana;
+
+/* #include <boost/hana/experimental/printable.hpp> */
+
+
+// let's try some template metaprogramming using boost Hana.
+// we need a metafunction to tell us whether or not list B
+// is a proper subset of list A so we know whether or not we
+// can safely do py_vector copy ctors and assigments when the 
+// py_vectors differ in the types they can hold.
+
+// the approach chosen is to use set operations
+// if the length of the intersection of the sets representing the type lists
+// used by each py_vector is equal to the length of the 'other' py_vector
+// then the members of the 'other' py_vector can be safely used by this py_vector.
+
+//https://lists.boost.org/boost-users/2007/06/28577.php
+//Than you for the quick reply :) Now I reimplemented 
+//intersect by converting the type sequences to mpl::set 
+//and do a mpl::has_key instead of mpl::contains: 
+//template <typename seq1, typename seq2> 
+//struct intersect 
+//{ 
+//  typedef typename copy<seq2 
+//     , inserter< set<>, insert< _1, _2 > > 
+//>::type set_seq2; 
+//  typedef typename copy_if<seq1 
+//     , has_key <set_seq2, _1> 
+//     , back_inserter < vector<> > 
+//>::type type; 
+//}; 
+
 
 /*
  * =====================================================================================
@@ -33,21 +69,54 @@
  * =====================================================================================
  */
 
-template <typename ...Ts>
+template<typename ...Ts>
 class py_vector
 {
     public:
 
+        static constexpr inline auto types_list_ = hana::to_tuple(hana::tuple_t<Ts...>);
+        static constexpr inline auto types_set_ = hana::make_set(hana::type_c<Ts>...);
+        
         using value_type = std::variant<Ts...>;
         using pylist_t = std::vector<value_type>;
 
+//        template <std::size_t N>
+//        using v_type = typename std::variant_alternative<N, std::variant<Ts...>>::type;
+
         /* ====================  LIFECYCLE     ======================================= */
         py_vector () { }                                                  /* constructor */
-        template<class ...Args>
+        template<typename ...Args>
         py_vector (Args ...args) : the_list_{{args} ...} { }              /* constructor */
 
         py_vector(const py_vector& rhs) : the_list_{rhs.the_list_} { }
         py_vector(py_vector&& rhs) : the_list_{std::move(rhs.the_list_)} { }
+
+        // now, let's try some metaprogramming....
+        // NOTE: per "C++ Templates the Complete Guide, 2nd ed." (pp.102,103), this is necessary to force use
+        // of customized copy ctor.
+        
+        py_vector(py_vector const volatile& rhs) = delete;
+
+        // now, make sure the we have all the types the class we are copying from does.
+        
+        template<class U,
+            typename = std::enable_if_t<hana::equal(hana::intersection(types_set_, U::types_set_), U::types_set_)>>
+        py_vector(const U& rhs)
+        {
+            // we can not simply copy the variant elements from the rhs vector, we need to use
+            // our own type of variant elements which, thanks to check above, we know can handle
+            // all the types possible in rhs.
+           
+            value_type elem;
+            auto get_value([this, &elem](const auto& e){ elem = e ; }); 
+
+            for (const auto& r_element : rhs.the_list_)
+            {
+                std::visit(get_value, r_element);
+                this->the_list_.push_back(elem);
+            }
+        }
+        template<typename ...Us> friend class py_vector;
 
         /* ====================  ACCESSORS     ======================================= */
 
@@ -104,7 +173,7 @@ class py_vector
         {
             if (this != & rhs)
             {
-                std::copy(rhs.the_list_.begin(), rhs.the_list_.end(), std::back_inserter(the_list_));
+                std::copy(rhs.the_list_.cbegin(), rhs.the_list_.cend(), std::back_inserter(the_list_));
             }
             return *this;
         }
@@ -112,7 +181,7 @@ class py_vector
         template<typename T>
         py_vector& append(const T& element)
         {
-            value_type e{std::in_place_type<T>, element};
+            value_type e(std::in_place_type<T>, element);
             the_list_.push_back(e);
             return *this;
         }
@@ -123,6 +192,15 @@ class py_vector
         {
             if (this != &rhs)
                 the_list_ = rhs.the_list_;
+            return *this;
+        }
+
+        template<typename U>
+        py_vector& operator=(const U& rhs)
+        {
+            // let's make sure all the types from rhs are available here.
+            
+
             return *this;
         }
 
@@ -137,6 +215,14 @@ class py_vector
         {
             if (this != &rhs)
                 this->append(rhs);
+            return *this;
+        }
+
+        template<typename T>
+        py_vector& operator+=(const T& element)
+        {
+            value_type e{std::in_place_type<T>, element};
+            the_list_.push_back(e);
             return *this;
         }
 
--- test2.cpp
+++ test2.cpp
@@ -35,65 +35,104 @@ int main(int argc, char *argv[])
     // see Bartek's coding blog vor article on std::variant
     
     py_vector<std::string, int, float> like_a_list; 
-    py_vector<int, std::string, float> like_a_list2{3, "ab", 5, 3.4f}; 
-
-    std::cout << "like_a_list.  should be empty.\n";
-    like_a_list.print_list(std::cout);
-
-    std::cout << "like_a_list2.  should be [3, ab, 5, 3.4].\n";
+    py_vector<int, std::string, float> like_a_list2{2, 3, "ab", 5, 3.4f}; 
+    /* like_a_list2.append(3).append("ab"s).append(5).append(3.4f); */ 
     like_a_list2.print_list(std::cout);
+
+    auto e = like_a_list2[0];
+    /* auto f = like_a_list2::v_type<like_a_list2.index()>; */
+
+
+
+
+
+
+
+
+    py_vector<double, int> like_a_list3{3, 4, 5, 6.2};
+    /* like_a_list3.append(3).append(4).append(5).append(6.2); */
+    like_a_list3.print_list(std::cout);
+
     
-    py_vector<int, std::string, float> like_a_list3{std::move(like_a_list2)}; 
+    std::cout << hana::length(like_a_list2.types_set_) << '\n';
+    std::cout << hana::length(like_a_list3.types_set_) << '\n';
+    /* auto common = hana::make_set(hana::intersection(py_vector<double, int>().t_list, py_vector<int, std::string, float>().t_list)); */
 
-    std::cout << "like_a_list2.  should be empty after move ctor.\n";
-    like_a_list2.print_list(std::cout);
+    constexpr auto xs = hana::make_set(hana::type_c<int>, hana::type_c<float>, hana::type_c<double>);
+    constexpr auto ys = hana::make_set(hana::type_c<int>, hana::type_c<int>,hana::type_c<float>);
+    std::cout << hana::length(hana::intersection(xs, ys)) << '\n';
 
-    std::cout << "like_a_list3.  should be [3, ab, 5, 3.4].\n";
-    like_a_list3.print_list(std::cout);
-     
-    py_vector<int, std::string, float> like_a_list4; 
-
-    std::cout << "like_a_list4.  should be empty.\n";
-    like_a_list4.print_list(std::cout);
-
-    like_a_list4 = like_a_list3;
-
-    std::cout << "like_a_list4.  should be [3, ab, 5, 3.4] after assignment.\n";
-    like_a_list4.print_list(std::cout);
-
-    like_a_list3.append(like_a_list4);
-
-    std::cout << "like_a_list3.  should be [3, ab, 5, 3.4, 3, ab, 5, 3.4] after append.\n";
-    like_a_list3.print_list(std::cout);
-
-    py_vector<int, std::string, float> like_a_list5; 
-
-    std::cout << "like_a_list5.  should be empty.\n";
+    std::cout << std::boolalpha << (hana::length(hana::intersection(like_a_list3.types_set_, like_a_list2.types_set_))
+       == hana::length(like_a_list3.types_set_)) << '\n';
+    
+    py_vector<float, int> like_a_list4{3, 3, 5, 6.2f};
+    /* like_a_list4.append(3).append(4).append(5).append(6.2f); */
+    py_vector<int, std::string, float> like_a_list5(like_a_list4);
     like_a_list5.print_list(std::cout);
 
-    like_a_list5 += like_a_list4;
-    std::cout << "like_a_list5.  should be [3, ab, 5, 3.4] after +=.\n";
-    like_a_list5.print_list(std::cout);
+    /* std::cout << std::boolalpha << a_contains_b(like_a_list2, like_a_list3) << '\n'; */
+    /* a_contains_b(like_a_list2, like_a_list3); */
+    /* auto x = driver(like_a_list2, like_a_list3); */
 
-//    like_a_list5.append('c');
-// should fail to compile -- type conversion restriced here.
-    like_a_list5.append("c"s);       // now, this should work
-    std::cout << "like_a_list5.  should be [3, ab, 5, 3.4, c] after append.\n";
-    like_a_list5.print_list(std::cout);
+    /* like_a_list2 = like_a_list3; */
 
-    auto like_a_list6 = like_a_list3.slice(3, 6);
-    std::cout << "like_a_list6.  should be [3.4, 3, ab] after slice.\n";
-    like_a_list6.print_list(std::cout);
-
-    like_a_list6[2] = 99;
-    std::cout << "like_a_list6.  should be [3.4, 3, 99] after element assignment.\n";
-    like_a_list6.print_list(std::cout);
-
-    std::cout << "test the 'contains' function.  should report 'false'\n";
-    std::cout << std::boolalpha << like_a_list6.contains("abcd"s) << '\n';
-
-    std::cout << "test the 'contains' function.  should report 'true'\n";
-    std::cout << std::boolalpha << like_a_list6.contains(3.4f) << '\n';
+//    std::cout << "like_a_list.  should be empty.\n";
+//    like_a_list.print_list(std::cout);
+//
+//    std::cout << "like_a_list2.  should be [3, ab, 5, 3.4].\n";
+//    like_a_list2.print_list(std::cout);
+//    
+//    py_vector<int, std::string, float> like_a_list3{std::move(like_a_list2)}; 
+//
+//    std::cout << "like_a_list2.  should be empty after move ctor.\n";
+//    like_a_list2.print_list(std::cout);
+//
+//    std::cout << "like_a_list3.  should be [3, ab, 5, 3.4].\n";
+//    like_a_list3.print_list(std::cout);
+//     
+//    py_vector<int, std::string, float> like_a_list4; 
+//
+//    std::cout << "like_a_list4.  should be empty.\n";
+//    like_a_list4.print_list(std::cout);
+//
+//    like_a_list4 = like_a_list3;
+//
+//    std::cout << "like_a_list4.  should be [3, ab, 5, 3.4] after assignment.\n";
+//    like_a_list4.print_list(std::cout);
+//
+//    like_a_list3.append(like_a_list4);
+//
+//    std::cout << "like_a_list3.  should be [3, ab, 5, 3.4, 3, ab, 5, 3.4] after append.\n";
+//    like_a_list3.print_list(std::cout);
+//
+//    py_vector<int, std::string, float> like_a_list5; 
+//
+//    std::cout << "like_a_list5.  should be empty.\n";
+//    like_a_list5.print_list(std::cout);
+//
+//    like_a_list5 += like_a_list4;
+//    std::cout << "like_a_list5.  should be [3, ab, 5, 3.4] after +=.\n";
+//    like_a_list5.print_list(std::cout);
+//
+////    like_a_list5.append('c');
+//// should fail to compile -- type conversion restriced here.
+//    like_a_list5.append("c"s);       // now, this should work
+//    std::cout << "like_a_list5.  should be [3, ab, 5, 3.4, c] after append.\n";
+//    like_a_list5.print_list(std::cout);
+//
+//    auto like_a_list6 = like_a_list3.slice(3, 6);
+//    std::cout << "like_a_list6.  should be [3.4, 3, ab] after slice.\n";
+//    like_a_list6.print_list(std::cout);
+//
+//    like_a_list6[2] = 99;
+//    std::cout << "like_a_list6.  should be [3.4, 3, 99] after element assignment.\n";
+//    like_a_list6.print_list(std::cout);
+//
+//    std::cout << "test the 'contains' function.  should report 'false'\n";
+//    std::cout << std::boolalpha << like_a_list6.contains("abcd"s) << '\n';
+//
+//    std::cout << "test the 'contains' function.  should report 'true'\n";
+//    std::cout << std::boolalpha << like_a_list6.contains(3.4f) << '\n';
 
     return 0;
 }
